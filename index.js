cb61376807e5227f582a0ebbb4eac75b82c2b069
Artem Bogdanov <temror@gmail.com>
2022-01-04 16:00:05 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log('No second')
 =======
 console.log('Master second')
 >>>>>>> Master commit
+=======
+console.log('Master second 2')
+>>>>>>> Master second commit
